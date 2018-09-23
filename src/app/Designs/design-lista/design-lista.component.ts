4bf5f6e83686de472cdba608df8dd6639def1ff6
AbdulBasitMohamedNor <abumer04@gmail.com>
2018-09-23 21:39:36 +0200

    homeSafe

---
+++ src/app/Designs/design-lista/design-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-design-lista',
+  templateUrl: './design-lista.component.html',
+  styleUrls: ['./design-lista.component.css']
+})
+export class DesignListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from './material/material/material.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DesignListaComponent } from './designs/design-lista/design-lista.component';
+import { DesignDetailComponent } from './designs/design-detail/design-detail.component';
+import { DesignsComponent } from './designs/designs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesignListaComponent,
+    DesignDetailComponent,
+    DesignsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
