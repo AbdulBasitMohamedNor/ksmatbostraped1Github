ab07ec33a75000c2bfd07761eea88cc2145ec3d7
AbdulBasitMohamedNor <abumer04@gmail.com>
2018-09-24 21:55:10 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DesignListaComponent } from './designs/design-lista/design-lista.component';
-import { DesignDetailComponent } from './designs/design-detail/design-detail.component';
-import { DesignsComponent } from './designs/designs.component';
+import { DesignListaComponent } from './Designs/design-lista/design-lista.component';
+import { DesignDetailComponent } from './Designs/design-detail/design-detail.component';
+import { DesignsComponent } from './Designs/designs.component';
 
 @NgModule({
   declarations: [
